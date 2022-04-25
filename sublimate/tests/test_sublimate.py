ec859014abc77ceb9548316fd5f5ca046a07aa3d
blake663 <blakesmudgepot@gmail.com>
2022-04-24 20:51:14 -0400

    finalized unit tests

--- sublimate/tests/test_sublimate.py
+++ sublimate/tests/test_sublimate.py
@@ -1,12 +1,15 @@
+ 
+import sys 
+sys.path.append('..')
+
 import pytest
 from sublimate.sublimate import Network
 import networkx as nx
 import json
 import math
  
- 
 def test_disconnected_graph():
-    with open('./test_data/disconnected_graph.json', 'r') as f:
+    with open('tests/disconnected_graph.json', 'r') as f:
         data = f.read()
     network = Network(data, ['212.209.235.224'], '135.22.112.220', {'diagramName': 'disconnected_graph'})
     network.Sublimate(2)
@@ -26,7 +29,6 @@ def test_complete_3_graph():
     expected_value = math.floor(math.e * math.factorial(size-2))
     graph = complete_graph(size)
     data = json.dumps(nx.readwrite.node_link_data(graph))
-    print(data)
     network = Network(data, ['0'], '1', {'diagramName': 'disconnected_graph'})
     network.Sublimate(math.factorial(size))
     assert len(network.victimNodes[0].compromisePaths) == expected_value
@@ -37,22 +39,29 @@ def test_complete_graphs():
         expected_value = math.floor(math.e * math.factorial(size-2))
         graph = complete_graph(size)
         data = json.dumps(nx.readwrite.node_link_data(graph))
-        print(data)
         network = Network(data, ['0'], '1', {'diagramName': 'disconnected_graph'})
         network.Sublimate(math.factorial(size))
         assert len(network.victimNodes[0].compromisePaths) == expected_value
 
  
 def test_find_0_paths():
-    with open('./test_data/demo_graph.json', 'r') as f:
+    with open('tests/demo_graph.json', 'r') as f:
         data = f.read()
     network = Network(data, ['212.209.235.224'], '135.22.112.220', {'diagramName': 'demo_graph'})
     network.Sublimate(0)
     assert len(network.victimNodes[0].compromisePaths) == 0
 
 def test_demo_graph():
-    with open('./test_data/demo_graph.json', 'r') as f:
+    with open('tests/demo_graph.json', 'r') as f:
         data = f.read()
     network = Network(data, ['212.209.235.224'], '135.22.112.220', {'diagramName': 'demo_graph'})
-    network.Sublimate(2)
-    assert len(network.victimNodes[0].compromisePaths) == 2
+    network.Sublimate(3)
+    paths = network.victimNodes[0].compromisePaths
+    print(paths[0].weight)
+    print(paths[1].weight)
+    print(paths[2].weight)
+
+    assert (len(paths) == 3 
+        and math.isclose(paths[0].weight, 1)
+        and math.isclose(paths[1].weight, 0.22948468672526573)
+        and math.isclose(paths[0].weight, 1))
